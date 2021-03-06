4208192bacd5c27352d9678105e4baf474677903
Christian Krüger <15275971+chrisnkrueger@users.noreply.github.com>
2021-03-06 10:03:34 +0100

    Initial Commit

---
+++ HitMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HitMe
+//
+//  Created by Christian Krüger on 06.03.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HitMe/HitMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HitMeApp.swift
+//  HitMe
+//
+//  Created by Christian Krüger on 06.03.21.
+//
+
+import SwiftUI
+
+@main
+struct HitMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
